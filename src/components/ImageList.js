241c0e16c0059d13adc4f0c74db68f9d9072784c
joe contreras <joe.contreras888@gmail.com>
2021-02-16 13:42:10 -0600

    conosle logs

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,10 @@ import "./ImageList.css";
 import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  console.log(props.images);
+  console.debug("este es un mensaje en debug");
+  console.warn("este es un mensaje de aviso");
+  console.error("este es un mensaje de error");
+  console.info("este es un mensaje de informacion");
   const images = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />;
   });
